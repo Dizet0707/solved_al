477ef47acf478c8ca529cd5ddc244e957fa5736a
Dizet_dev <dizet0707@pm.me>
2025-02-19 21:57:20 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,20 @@
+// 1. 시간복잡도가 뭐임?
+// 2. MenOfPassion 알고리즘이 뭐임? ;; 아 함수를 만들어서 그냥 제공해준거구나 출제자 이름이네
+// 3. A 배열이랑 a parameter을 받아서... i를 n/2로 만들고, A배열 중 그 i번째 idx값을 return.
+// 4. 시간복잡도...는... parameter 2개를 받고, 실질적으로 메모리에 접근하는 건 1번. 1?
+// 5. 수행횟수를 방정식으로 했을 때 최고차항의 차수? 뭘 넣어도 무조건 1나오는 거면 0번째 차수인가?
+// 6. 근데 이게 어차피 1, 0인데 입력이 필요하나?
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    //     int a;
+    //     cin >> a;
+
+    cout << 1 << endl
+         << 0 << endl;
+
+    return 0;
+}
