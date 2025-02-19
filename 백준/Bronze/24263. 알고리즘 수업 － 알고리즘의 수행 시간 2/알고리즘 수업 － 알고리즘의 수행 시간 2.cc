72832a58818ab74c7dae4ee5b3d3a25e92ee0826
Dizet_dev <dizet0707@pm.me>
2025-02-19 22:10:47 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,17 @@
+// 1. 수도코드 나오는 문제는 처음보는데
+// 2. sum = 0, i가 1부터 시작해서 n까지 반복...? 이럼 n번 반복...? 이하인지 미만인지 말해줘야...
+// 3. sum에 A의 i번째 idx의 값을 추가함. 이러면 2번 아님?
+// 4. 아 for문이 있으니까 총 몇 번 실행되느냐에 따라서 달라지구나. 그러면 최고차항은 x아님?
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int32_t a; // ㄷㄷ 이거 그냥 추가되노
+    cin >> a;
+    cout << a << endl
+         << 1 << endl;
+
+    return 0;
+}
