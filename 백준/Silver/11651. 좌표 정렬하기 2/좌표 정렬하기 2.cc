8c63562cd57cb31a814196d3b7dc7f356a9f9509
Dizet_dev <dizet0707@pm.me>
2025-03-18 01:32:43 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 40 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -1,36 +1,41 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
-bool compare(pair<int, int>a, pair<int, int>b)
-{
-	if (a.second == b.second) {
-		return a.first < b.first;
-	}
+// ㅅㅂ 다 갖다버려; 구현해도 풀리지 않는데 이게 뭐노;
+#include <algorithm>
+#include <vector>
+// using namespace std;
 
-	else {
-		return a.second < b.second;
-	}
+struct interval
+{
+    int x, y;
+};
+
+bool compare(const interval &a, const interval &b) // const는 하는 게 좋다고 하네; 기억용
+{
+    if (a.y == b.y)
+        return a.x < b.x;
+    return a.y < b.y;
 }
 
 int main()
 {
-	int n, x, y;
-	
-	cin >> n;
-	vector<pair<int, int>> vec(n);
-	for (int i = 0; i < n; i++) {
-		cin >> x >> y;
-		vec[i].first = x;
-		vec[i].second = y;
-	}
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); // 조금이라도 빠르게;; 이런 게 있는지도 몰랐노;
+    cout.tie(NULL);
 
-	sort(vec.begin(), vec.end(), compare);
+    int n;
+    cin >> n;
+    vector<interval> intervals(n);
 
-	for (int i = 0; i < n; i++) {
-		printf("%d %d\n", vec[i].first, vec[i].second);
-	}
+    for (int i = 0; i < n; i++)
+        cin >> intervals[i].x >> intervals[i].y;
 
-	return 0;
+    sort(intervals.begin(), intervals.end(), compare); // compare 자체가 return값이 bool이기에 생기는 일. 근데 어떻게 a랑 b가 넘어가지?
+
+    // for (const auto &p : intervals)
+    for (int i = 0; i < n; i++)
+        cout << intervals[i].x << ' ' << intervals[i].y << '\n';
+
+    return 0;
 }
